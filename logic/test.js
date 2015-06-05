0699e704cadd859ba75cd16effc87896e2d33a2d
ZhenfeiNie <zhenfei.nie@gmail.com>
2015-06-05 22:04:35 +0200

    set up basic framework

---
+++ logic/test.js
@@ -0,0 +1,17 @@
+var WT = {
+  /**
+   * TODO
+   * 
+   * @method  getAllTimestamps
+   * @return array array of timestamps
+   */
+  getAllTimestamps: function() {
+    return [1, 2]
+  }
+}
+
+$(document).ready(function() {
+  var x = WT.getAllTimestamps()
+  console.log('al', x);
+});
+
---
+++ popup/popup.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  chrome.tabs.getSelected(null, function(tab) {
+    var tablink = tab.url;
+    console.log('tablink', tablink);
+    $('#url').html(tablink);
+  });
+});
