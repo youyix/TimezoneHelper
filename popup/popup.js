24d65e4e8c13ff96955d8a9badd9bf2ec8e49569
ZhenfeiNie <zhenfei.nie@gmail.com>
2015-06-07 00:39:37 +0200

    popup/: use storge to store preferences

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,15 @@
+/**
+ * @author  youyix<youyis2fox@gmail.com>
+ */
+
 var timezoneChanged = function(event) {
   var timezone = this.value;
 
   $('#value').text(timezone);
+
+  chrome.storage.sync.set({'timezone': timezone}, function() {
+    console.log('stored', timezone);
+  })
 
   var data = {
     type: 'TIMEZONE_CHANGE',
@@ -20,13 +28,36 @@ var timezoneChanged = function(event) {
   })
 }
 
+
+var getTimezone = function() {
+  return $('#sel').val();
+}
+
+var setTimezone = function(timezone) {
+  $('#sel').select2().select2('val', timezone);
+  $('#value').text(timezone);
+  
+}
+
 $(document).ready(function() {
   $("#sel").select2({
     data: timeData
   })
-  $('#value').text($('#sel').val());
+  $('#value').text(getTimezone());
 
   $('#sel').on('select2:select', timezoneChanged);
+
+  // Get stored timezone value in chrome storage
+  chrome.storage.sync.get('timezone', function(items) {
+    if ( items.hasOwnProperty('timezone') ) {
+      console.log('loaded', items['timezone']);
+      setTimezone(items['timezone'])
+    } else {
+      chrome.storage.sync.set({'timezone': getTimezone()}, function() {
+        console.log('stored', getTimezone());
+      })
+    }
+  });
 });
 
 
